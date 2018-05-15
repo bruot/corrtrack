bruot/corrtrack

commit 931b459b37ce73d33e62541e27e66f737093685e

Highlight under- and over-exposed pixels + Settings storage

src/corrtrackwindow.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the particle tracking software CorrTrack.
  *
- * Copyright 2016, 2017 Nicolas Bruot
+ * Copyright 2016-2018 Nicolas Bruot
  *
  *
  * CorrTrack is free software: you can redistribute it and/or modify
@@ -20,6 +20,7 @@
 
 
 #include <QApplication>
+#include <QCloseEvent>
 #include <QStatusBar>
 #include <QGraphicsView>
 #include <QGraphicsPixmapItem>
@@ -40,6 +41,8 @@
 #include <QMessageBox>
 #include <QMenuBar>
 #include <QImage>
+#include <QVector>
+#include <QRgb>
 #include <QFileInfo>
 #include <QGraphicsSceneWheelEvent>
 #include <QTimer>
@@ -51,6 +54,7 @@
 #include "intensitydialog.h"
 #include "zoomdialog.h"
 #include "corrfilterdialog.h"
+#include "settingsdialog.h"
 #include "math/math.h"
 #include "math/corrfilter.h"
 #include "math/point.h"
@@ -73,6 +77,7 @@ CorrTrackWindow::CorrTrackWindow(QWidget *parent)
       bitDepth{8},
       intensityMin{0},
       intensityMax{255},
+      colorTable(QVector<QRgb>(256)),
       zoomIndex{0},
       filterFile{QString()},
       currentTime{0.0},
@@ -102,6 +107,8 @@ CorrTrackWindow::CorrTrackWindow(QWidget *parent)
       speedCBox{new QComboBox(this)},
       frameIndexLabel{new QLabel(this)}
 {
+    settings = new Settings();
+
     const int squareButtonWidth = 40;
 
     pixmapItem->setAcceptHoverEvents(true);
@@ -165,6 +172,7 @@ CorrTrackWindow::CorrTrackWindow(QWidget *parent)
     w->setLayout(layout);
     setCentralWidget(w);
 
+    updateColorTable();
     updateZoom();
 
     createActions();
@@ -274,6 +282,12 @@ bool CorrTrackWindow::eventFilter(QObject *target, QEvent *event)
         }
     }
     return QMainWindow::eventFilter(target, event);
+}
+
+void CorrTrackWindow::closeEvent(QCloseEvent *event)
+{
+    settings->saveSettings();
+    event->accept();
 }
 
 void CorrTrackWindow::addPoint(Point point)
@@ -563,6 +577,16 @@ void CorrTrackWindow::updateFrameIndex(const int frame)
     updateFrameDisplay();
 }
 
+void CorrTrackWindow::updateColorTable()
+{
+    for (int i = 0; i < 256; i++)
+        colorTable[i] = qRgb(i, i, i);
+    if (settings->highlightMinIntensity && intensityMode != IntensityMode::AutoVariable)
+        colorTable[0] = qRgb(0, 0, 255);
+    if (settings->highlightMaxIntensity && intensityMode != IntensityMode::AutoVariable)
+        colorTable[255] = qRgb(255, 0, 0);
+}
+
 void CorrTrackWindow::updateFrameDisplay()
 {
     if (movieIsSet)
@@ -585,8 +609,8 @@ void CorrTrackWindow::updateFrameDisplay()
         QImage image = QImage(data,
                               analyser->movie->width,
                               analyser->movie->height,
-                              analyser->movie->width,
                               QImage::Format_Indexed8);
+        image.setColorTable(colorTable);
         pixmapItem->setPixmap(QPixmap::fromImage(image));
         delete[] data;
         frameIndexLabel->setText(QString("%1 / %2").arg(currentFrameIndex).arg(analyser->movie->nFrames));
@@ -679,6 +703,31 @@ void CorrTrackWindow::closeMovie()
     fileNameLabel->setText("");
 
     setMovieRelatedItemsEnabled(false);
+}
+
+void CorrTrackWindow::editSettings()
+{
+    bool oldHighlightMinIntensity = settings->highlightMinIntensity;
+    bool oldHighlightMaxIntensity = settings->highlightMaxIntensity;
+
+    // Show settings dialog
+    SettingsDialog *dialog = new SettingsDialog(oldHighlightMinIntensity,
+                                                oldHighlightMaxIntensity,
+                                                this);
+
+    if (dialog->exec() == QDialog::Accepted)
+    {
+        settings->highlightMinIntensity = dialog->getHighlightMinIntensity();
+        settings->highlightMaxIntensity = dialog->getHighlightMaxIntensity();
+    }
+
+    if ((oldHighlightMinIntensity != settings->highlightMinIntensity)
+        | (oldHighlightMaxIntensity != settings->highlightMaxIntensity))
+    {
+        statusBar()->showMessage(tr("Settings changed."));
+        updateColorTable();
+        updateFrameDisplay();
+    }
 }
 
 bool CorrTrackWindow::validateCorrelation() const
@@ -877,6 +926,7 @@ void CorrTrackWindow::intensity()
             || intensityMax != oldIntensityMax)
     {
         statusBar()->showMessage(QString("Intensity settings changed."));
+        updateColorTable();
         updateFrameDisplay();
     }
 }
@@ -985,7 +1035,7 @@ void CorrTrackWindow::about()
         message.append(TARGET_VERSION);
         message.append(")");
     }
-    message.append("<br/><br/>Copyright (C) 2016, 2017 Nicolas Bruot<br/><br/>"
+    message.append("<br/><br/>Copyright (C) 2016-2018 Nicolas Bruot<br/><br/>"
                    "CorrTrack is released under the terms of the GNU General Public License (GPL) v3.<br/>"
                    "The source code is available at <a href=\"https://github.com/bruot/corrtrack/\">https://github.com/bruot/corrtrack/</a>.<br/><br/>"
                    "This program uses:"
@@ -1038,6 +1088,10 @@ void CorrTrackWindow::createActions()
     closeAct->setStatusTip(tr("Close the current file"));
     connect(closeAct, SIGNAL(triggered()), this, SLOT(closeMovie()));
 
+    settingsAct = new QAction(tr("Se&ttings"), this);
+    settingsAct->setStatusTip(tr("Program settings"));
+    connect(settingsAct, SIGNAL(triggered()), this, SLOT(editSettings()));
+
     exitAct = new QAction(tr("E&xit"), this);
     exitAct->setShortcuts(QKeySequence::Quit);
     exitAct->setStatusTip(tr("Exit the application"));
@@ -1084,6 +1138,8 @@ void CorrTrackWindow::createMenus()
     fileMenu->addAction(extractTiffsAct);
     fileMenu->addSeparator();
     fileMenu->addAction(closeAct);
+    fileMenu->addSeparator();
+    fileMenu->addAction(settingsAct);
     fileMenu->addSeparator();
     fileMenu->addAction(exitAct);
 

corrtrack.pro
@@ -1,6 +1,6 @@
 # This file is part of the particle tracking software CorrTrack.
 #
-# Copyright 2016, 2017 Nicolas Bruot
+# Copyright 2016-2018 Nicolas Bruot
 #
 #
 # CorrTrack is free software: you can redistribute it and/or modify
@@ -100,8 +100,10 @@ SOURCES += \
     main.cpp \
     corrtrackwindow.cpp \
     constants.cpp \
+    settings.cpp \
     zoomdialog.cpp \
     corrfilterdialog.cpp \
+    settingsdialog.cpp \
     okcanceldialog.cpp \
     movie/base/frame.cpp \
     math/math.cpp \
@@ -124,8 +126,10 @@ SOURCES += \
 HEADERS += \
     corrtrackwindow.h \
     constants.h \
+    settings.h \
     zoomdialog.h \
     corrfilterdialog.h \
+    settingsdialog.h \
     okcanceldialog.h \
     movie/base/frame.h \
     math/math.h \

src/constants.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the particle tracking software CorrTrack.
  *
- * Copyright 2016, 2017 Nicolas Bruot
+ * Copyright 2016-2018 Nicolas Bruot
  *
  *
  * CorrTrack is free software: you can redistribute it and/or modify
@@ -31,6 +31,7 @@ namespace constants
     const char* const VERSION = "2.9";
     const char* const TARGET_VERSION = "2.10";
 
+    const char* const ORGANIZATION = "CorrTrack";
     const char* const APP_NAME = "CorrTrack";
 
     const double ZOOM_BASE = 4.0 / 3.0;

src/constants.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the particle tracking software CorrTrack.
  *
- * Copyright 2016, 2017 Nicolas Bruot
+ * Copyright 2016-2018 Nicolas Bruot
  *
  *
  * CorrTrack is free software: you can redistribute it and/or modify
@@ -31,6 +31,7 @@ namespace constants
     extern const char* const VERSION;
     extern const char* const TARGET_VERSION;
 
+    extern const char* const ORGANIZATION;
     extern const char* const APP_NAME;
 
     extern const double ZOOM_BASE;

src/corrtrackwindow.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the particle tracking software CorrTrack.
  *
- * Copyright 2016, 2017 Nicolas Bruot
+ * Copyright 2016-2018 Nicolas Bruot
  *
  *
  * CorrTrack is free software: you can redistribute it and/or modify
@@ -34,12 +34,16 @@
 #include <QSlider>
 #include <QObject>
 #include <QEvent>
+#include <QCloseEvent>
 #include <QTimer>
 #include <QThread>
 #include <QString>
+#include <QVector>
+#include <QRgb>
 #include <QWidget>
 #include <QGraphicsEllipseItem>
 #include "noscrollqgraphicsview.h"
+#include "settings.h"
 #include "movie/movie.h"
 #include "math/corrtrackanalyser.h"
 #include "openmovieworker.h"
@@ -60,8 +64,11 @@ public:
     };
 
 private:
+    Settings *settings;
+
     bool movieIsSet;
     IntensityMode intensityMode;
+    QVector<QRgb> colorTable;
     unsigned int bitDepth;
     uint16_t intensityMin;
     uint16_t intensityMax;
@@ -113,6 +120,7 @@ private:
     QAction *extractCurrentTiffAct;
     QAction *extractTiffsAct;
     QAction *closeAct;
+    QAction *settingsAct;
     QAction *exitAct;
     // View
     QAction *intensityAct;
@@ -140,6 +148,7 @@ private:
     void createMenus();
     void setMovieRelatedItemsEnabled(const bool state);
     void displayMessageBox(const QString text) const;
+    void updateColorTable();
     void updateFrameDisplay();
     int frameCoordinate(const int coordinate) const;
     int zoomedCoordinate(const int coordinate) const;
@@ -152,6 +161,7 @@ private slots:
     void extractCurrentTiff();
     void extractTiffs();
     void closeMovie();
+    void editSettings();
     // View
     void intensity();
     void zoom();
@@ -179,4 +189,5 @@ public:
 
 protected:
     virtual bool eventFilter(QObject *target, QEvent *event) override;
+    void closeEvent(QCloseEvent *event) override;
 };

src/movie/movie.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the particle tracking software CorrTrack.
  *
- * Copyright 2016, 2017 Nicolas Bruot
+ * Copyright 2016-2018 Nicolas Bruot
  *
  *
  * CorrTrack is free software: you can redistribute it and/or modify
@@ -612,7 +612,7 @@ uint8_t* Movie::frameData8(const size_t i,
     int bitsShift = customBitDepth - 8;
     if (bitsPerSample == 8 && customBitDepth == 8)
         std::memcpy(data, frames8.at(i).pixelsData, sz);
-    else if (bitsPerSample == 8) // bitShift != 0
+    else if (bitsPerSample == 8) // bitsShift != 0
     {
         uint8_t* data8 = frames8.at(i).pixelsData;
         if (bitsShift > 0)
@@ -620,20 +620,20 @@ uint8_t* Movie::frameData8(const size_t i,
                 data[k] = data8[k] >> bitsShift;
         else // bitsShift < 0
             for (unsigned int k = 0; k < width * height; k++)
-                data[k] = data8[k] << -bitsShift;
+                data[k] = (data8[k] < (1 << customBitDepth)) ? data8[k] << -bitsShift : 255;
     }
     else // bitsPerSample = 16
     {
         uint16_t* data16 = frames16.at(i).pixelsData;
         if (bitsShift == 0)
             for (unsigned int k = 0; k < width * height; k++)
-                data[k] = (uint8_t) (data16[k]);
+                data[k] = (data16[k] < 1 << customBitDepth) ? (uint8_t) (data16[k]) : 255;
         else if (bitsShift > 0)
             for (unsigned int k = 0; k < width * height; k++)
-                data[k] = (uint8_t) (data16[k] >> bitsShift);
+                data[k] = (data16[k] < (1 << customBitDepth)) ? (uint8_t) (data16[k] >> bitsShift) : 255;
         else // bitsShift < 0
             for (unsigned int k = 0; k < width * height; k++)
-                data[k] = (uint8_t) (data16[k] << -bitsShift);
+                data[k] = (data16[k] < (1 << customBitDepth)) ? (uint8_t) (data16[k] << -bitsShift) : 255;
     }
     return data;
 }
@@ -647,23 +647,35 @@ uint8_t* Movie::frameData8(const size_t i,
     if (bitsPerSample == 8)
     {
         uint8_t* data8 = frames8.at(i).pixelsData;
-        uint8_t amplitude = maxValue - minValue;
+        int16_t amplitude = maxValue - minValue;
         for (unsigned int k = 0; k < width * height; k++)
         {
-            // Data type needs to hold 255 * uint8_t. Therefore, use uint16_t.
-            uint16_t val = (uint16_t) 255 * (data8[k] - minValue) / amplitude;
-            data[k] = (uint8_t) (val);
+            // Data type needs to hold 255 * uint8_t and negative values.
+            // Therefore, use int16_t.
+            int16_t val = 255 * (int16_t) (data8[k] - minValue) / amplitude;
+            if (val < 0)
+                data[k] = 0;
+            else if (val < 256)
+                data[k] = (uint8_t) (val);
+            else
+                data[k] = 255;
         }
     }
     else // bitsPerSample = 16
     {
         uint16_t* data16 = frames16.at(i).pixelsData;
-        uint16_t amplitude = maxValue - minValue;
+        int32_t amplitude = maxValue - minValue;
         for (unsigned int k = 0; k < width * height; k++)
         {
-            // Data type needs to hold 255 * uint16_t. Therefore, use uint32_t.
-            uint32_t val = (uint32_t) 255 * (data16[k] - minValue) / amplitude;
-            data[k] = (uint8_t) (val);
+            // Data type needs to hold 255 * uint16_t and negative values.
+            // Therefore, use int32_t.
+            int32_t val = 255 * (int32_t) (data16[k] - minValue) / amplitude;
+            if (val < 0)
+                data[k] = 0;
+            else if (val < 256)
+                data[k] = (uint8_t) (val);
+            else
+                data[k] = 255;
         }
     }
     return data;

src/settings.cpp
@@ -1,0 +1,59 @@
+/*
+ * This file is part of the particle tracking software CorrTrack.
+ *
+ * Copyright 2018 Nicolas Bruot
+ *
+ *
+ * CorrTrack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CorrTrack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CorrTrack.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <QVariant>
+#include <QMessageBox>
+#include <QSettings>
+#include "constants.h"
+#include "settings.h"
+
+
+Settings::Settings(QObject *parent) : QObject(parent)
+{
+    qsettings = new QSettings(QSettings::IniFormat, QSettings::UserScope,
+                              constants::ORGANIZATION, constants::APP_NAME);
+    highlightMinIntensity = getValue("Display/HighlightMinIntensity", true);
+    highlightMaxIntensity = getValue("Display/HighlightMaxIntensity", true);
+}
+
+
+Settings::~Settings()
+{
+    delete qsettings;
+}
+
+template<typename T>
+    T Settings::getValue(QString field, T defaultValue)
+{
+    QVariant val = qsettings->value(field, defaultValue);
+    if (val.canConvert<T>())
+    {
+        if (val.convert(qMetaTypeId<T>()))
+            return val.value<T>();
+    }
+    return defaultValue;
+}
+
+void Settings::saveSettings()
+{
+    qsettings->setValue("Display/HighlightMinIntensity", highlightMinIntensity);
+    qsettings->setValue("Display/HighlightMaxIntensity", highlightMaxIntensity);
+}

src/settings.h
@@ -1,0 +1,48 @@
+/*
+ * This file is part of the particle tracking software CorrTrack.
+ *
+ * Copyright 2018 Nicolas Bruot
+ *
+ *
+ * CorrTrack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CorrTrack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CorrTrack.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#pragma once
+
+
+#include <QString>
+#include <QSettings>
+
+
+class Settings : public QObject
+{
+    Q_OBJECT
+
+public:
+    Settings(QObject *parent = 0);
+    ~Settings();
+
+    bool highlightMinIntensity;
+    bool highlightMaxIntensity;
+
+public slots:
+    void saveSettings();
+
+private:
+    QSettings *qsettings;
+
+    template<typename T>
+        T getValue(QString field, T defaultValue);
+};

commit ae0f58adfcb5e23a1ca8b9527aa15b570c67858b

Display images with small width correctly
This fixes a regression introduced by
931b459b37ce73d33e62541e27e66f737093685e.

src/corrtrackwindow.cpp
@@ -584,6 +584,7 @@ void CorrTrackWindow::updateFrameDisplay()
         QImage image = QImage(data,
                               analyser->movie->width,
                               analyser->movie->height,
+                              analyser->movie->width,
                               QImage::Format_Indexed8);
         image.setColorTable(colorTable);
         pixmapItem->setPixmap(QPixmap::fromImage(image));